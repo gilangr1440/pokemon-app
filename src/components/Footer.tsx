9f5a73d46ddfd9e4410b40eb8168487e69eca5fa
gilangr1440 <gilangr1440@gmail.com>
2024-05-12 16:18:47 +0700

    create layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import { FaHome } from "react-icons/fa";
+import { MdCatchingPokemon } from "react-icons/md";
+
+const Footer = () => {
+  return (
+    <footer className="w-full h-20 grid grid-cols-2 bg-black">
+      <div className="text-white flex flex-col justify-center items-center">
+        <FaHome className="text-4xl" />
+        <span>Home</span>
+      </div>
+      <div className="text-white flex flex-col justify-center items-center">
+        <MdCatchingPokemon className="text-4xl" />
+        <span>My Pokemon</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
